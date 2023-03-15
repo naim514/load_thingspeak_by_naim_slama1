ec7250100b19b774a54cf536462b46fa11633f54
naim <96814060+naim514@users.noreply.github.com>
2023-03-15 18:59:53 +0100

    Mise à jour pxt.json, main.blocks, main.ts, naimhello.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-let l = ""
+WiFiIoT.on_wifi_connect(function (IP_Address, Device_ID) {
+    basic.showString(IP_Address)
+})
 input.onButtonPressed(Button.A, function () {
     WiFiIoT.sendGenericHttp(
     WiFiIoT.httpMethod.GET,
@@ -10,3 +12,6 @@ WiFiIoT.on_HTTP_recevid(function (HTTP_Status_Code, Data) {
     l = WiFiIoT.get_value("feeds", Data)
     basic.showString(WiFiIoT.get_value("field1", l[0]))
 })
+let l = ""
+WiFiIoT.initializeWifi(SerialPin.P12, SerialPin.P8)
+WiFiIoT.setWifi("Ooredoo 4G_DAE", "21554265")
